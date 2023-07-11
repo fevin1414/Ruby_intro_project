4b4286545d667faff87e3a20ecfbd6af93f610d7
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-11 13:30:05 -0500

    added validation

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :match
+
+  validates :player_name, :minute, presence: true
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,2 +1,5 @@
 class Match < ApplicationRecord
+  has_many :goals
+
+  validates :date, :home_team, :away_team, :stadium, presence: true
 end
