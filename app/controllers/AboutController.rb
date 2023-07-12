a36c064dbbb241643c3c2c734904eb1aa53fa70b
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-12 00:19:08 -0500

    added about page

---
+++ app/controllers/AboutController.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+  def index
+    # Add any necessary logic or data retrieval for the About page here
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'matches#index'
   resources :matches, only: [:index, :show]
-  get '/about', to: 'matches#about'
+  get '/about', to: 'about#index'
 end
