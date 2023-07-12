7f1fba46b675b8ab80ac544d0ef0140aa542da2f
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-11 20:22:42 -0500

    added menu and home page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -6,4 +6,6 @@ class MatchesController < ApplicationController
   def show
     @match = Match.find(params[:id])
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'matches#index'
   resources :matches, only: [:index, :show]
+  get '/about', to: 'matches#about'
 end
