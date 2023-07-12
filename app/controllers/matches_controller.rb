324d30a4565d2f4f2bfa30b4c6fa1ef7b1c15bee
Fevin Biju <fevinbiju1414@gmail.com>
2023-07-12 17:23:31 -0500

    added search

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,15 @@ class MatchesController < ApplicationController
   def index
     @matches = Match.paginate(page: params[:page], per_page: 50)
 
+
+    if params[:search].present?
+      @matches = Match.where("home_team LIKE :search OR away_team LIKE :search", search: "%#{params[:search]}%")
+                     .paginate(page: params[:page], per_page: 50)
+    else
+      @matches = Match.paginate(page: params[:page], per_page: 50)
+    end
+
+
   end
 
   def show
